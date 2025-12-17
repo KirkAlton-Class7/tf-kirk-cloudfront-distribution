24b9662694962c8f3819de2bd366ac89ed2c49ca
KirkAlton-Class7 <232495916+KirkAlton-Class7@users.noreply.github.com>
2025-12-16 19:43:06 -0600

    first commit

--- 02-bucket-config.tf
+++ 02-bucket-config.tf
@@ -1,70 +1,35 @@
-resource "aws_s3_bucket_website_configuration" "website" {
-  bucket = aws_s3_bucket.website.id
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
-  }
-}
-
-
-##########
-
-
 resource "aws_s3_bucket_public_access_block" "website" {
   bucket = aws_s3_bucket.website.id
 
-  block_public_acls       = false
+  block_public_acls       = true
   ignore_public_acls      = true
  
-  block_public_policy     = false
-  restrict_public_buckets = false
+  block_public_policy     = true
+  restrict_public_buckets = true
 }
 
-
-
 ######
-
-resource "aws_s3_bucket_policy" "allow_public_access_for_all_objects" {
+resource "aws_s3_bucket_policy" "cloudfront_access" {
   bucket = aws_s3_bucket.website.id
   policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "PublicAccess"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.website.arn}/*"
-      }
-    ]
+    # Statement = [
+    #   {
+    #     Sid    = "AllowFaccess"
+    #     Effect = "Allow"
+    #     Principal = {
+    #       Service = "cloudfront.amazonaws.com"
+    #     }
+    #     Action   = "s3:GetObject"
+    #     Resource = "${aws_s3_bucket.website.arn}/*"
+    #     Condition = {
+    #       StringEquals = {
+    #         "AWS:SourceArn" = "${aws_cloudfront_distribution.website.arn}"
+    #       }
+    #     }
+    #   }
+    # ]
   })
-}
-
-
-
-
-
-
-
-data "aws_iam_policy_document" "allow_access_from_another_account" {
-  statement {
-    principals {
-      type        = "AWS"
-      identifiers = ["123456789012"]
-    }
-
-    actions = [
-      "s3:GetObject",
-      "s3:ListBucket",
-    ]
-
-    resources = [
-      aws_s3_bucket.website.arn,
-      "${aws_s3_bucket.website.arn}/*",
-    ]
-  }
+   # add this explicit dependency, dependency chain issues occur intermittently
+  depends_on = [aws_s3_bucket_public_access_block.website]
 }
--- 999-output.tf
+++ 999-output.tf
@@ -1,15 +1,15 @@
-output "website_url" {
-  value       = "http://${aws_s3_bucket_website_configuration.website.website_endpoint}"
-  description = "The URL of the static website hosted on the S3 bucket"
-}
-output "bucket_name" {
-  value       = aws_s3_bucket.website.bucket
+# output "website_url" {
+#   value       = "http://${aws_s3_bucket_website_configuration.website.website_endpoint}"
+#   description = "The URL of the static website hosted on the S3 bucket"
+# }
+
+output "bucket_info" {
+  value = {
+    name = aws_s3_bucket.website.bucket
+    arn = aws_s3_bucket.website.arn
+  }
 }
 
-output "bucket_arn" {
-  value       = aws_s3_bucket.website.arn
-}
-
-output "policy" {
-  value       = aws_s3_bucket_policy.allow_public_access_for_all_objects.policy
-}
+# output "policy" {
+#   value       = aws_s3_bucket_policy.allow_public_access_for_all_objects.policy
+# }
